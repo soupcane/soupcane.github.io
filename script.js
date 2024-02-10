8b5d4e443c6be4eb8a959b21dffef84832c958a5
Leon Svensson <ltksvensson@gmail.com>
2024-02-10 15:35:33 +0100

    fix

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
 const galleryContainer = document.querySelector('.gallery-container');
 const imageBox = document.querySelectorAll('.image-box');
-const modal = document.createElement('div');
-const modalImg = document.createElement('img');
-
-modal.classList.add('modal');
-modal.appendChild(modalImg);
-document.body.appendChild(modal);
+const modalImg = document.getElementById('overlayImg');
 
 imageBox.forEach((box) => {
   box.addEventListener('click', () => { 
     const imgSrc = box.querySelector('img').getAttribute('src'); 
     modalImg.setAttribute('src', imgSrc); 
-    modalImg.style.width = '50%';
-    modal.style.display = 'block'; 
-    modal.style.paddingTop = '20px';
+    document.getElementById("overlay").style.display = "block";
   });
-
-  box.addEventListener('click', () => { 
-    const imgSrc = box.querySelector('img').getAttribute('src'); 
-    modalImg.setAttribute('src', imgSrc); modal.style.display = 'block'; 
-  });
-
-  modal.addEventListener('click', () => { 
-    modal.style.display = 'none'; 
-  }); 
 });
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
